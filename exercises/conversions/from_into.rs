1abdc4c05c9004141bcfa95a9b599b4d3e6207bc
Jones-Wu <3539754091@qq.com>
2024-03-17 13:08:26 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let items:Vec<&str> = s.split(",").collect();
+
+        if items.len() != 2 || items[0].len() <= 0 || items[1].len() <= 0{
+            return Person::default();
+        }
+
+        for c in items[1].chars() {
+            if !c.is_ascii_digit(){
+                return Person::default();
+            }
+        }
+
+        Person{
+            name: items[0].to_string(),
+            age: items[1].parse::<usize>().unwrap(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,8 +51,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let items:Vec<&str> = s.split(",").collect();
+
+        if items.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        
+        if items[0].len() <= 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        match items[1].parse::<usize>(){
+            Ok(n) => Ok(Person{name: items[0].to_string(), age: n,}),
+            Err(e) => return Err(ParsePersonError::ParseInt(e))
+        }
     }
 }
+
 
 fn main() {
     let p = "Mark,20".parse::<Person>().unwrap();
